606ef4c547b25c868eb3766b2c99c0500e27d211
damian_qqx <damian_qqx@meten.com>
2018-12-19 22:25:28 +0800

    db first

---
+++ TIEDPAG.Dispense.DAL/Area.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TIEDPAG.Dispense.DAL
+{
+    public partial class Area
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Createby { get; set; }
+        public DateTime CreateTime { get; set; }
+        public int? Updateby { get; set; }
+        public DateTime? UpdateTime { get; set; }
+        public string Remark { get; set; }
+    }
+}
---
+++ TIEDPAG.Dispense.DAL/tiedpag_dispenseContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace TIEDPAG.Dispense.DAL
+{
+    public partial class tiedpag_dispenseContext : DbContext
+    {
+        public tiedpag_dispenseContext()
+        {
+        }
+
+        public tiedpag_dispenseContext(DbContextOptions<tiedpag_dispenseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Area> Area { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySQL("Server=182.61.40.185;User Id=tiedpag;Password=Mysql@123;Database=tiedpag_dispense");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.0-rtm-35687");
+
+            modelBuilder.Entity<Area>(entity =>
+            {
+                entity.ToTable("Area", "tiedpag_dispense");
+
+                entity.HasIndex(e => e.Name)
+                    .HasName("Area_Index_Name");
+
+                entity.Property(e => e.Id)
+                    .HasColumnType("int(11)")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Createby).HasColumnType("int(11)");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(120)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Remark)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Updateby).HasColumnType("int(11)");
+            });
+        }
+    }
+}
